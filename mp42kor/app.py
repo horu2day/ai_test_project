1d2e76abc3921ac12d08d0b6399ed5abc9eb035a
김민성 <kms3700@gmail.com>
2024-05-24 19:36:46 +0900

    splite text

--- mp42kor/app.py
+++ mp42kor/app.py
@@ -7,6 +7,31 @@ from deep_translator import GoogleTranslator
 import time
 
 
+def split_text(file_path, max_length=5000):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        text = file.read()
+
+    parts = []
+    while text:
+        part = text[:max_length]
+        next_index = part.rfind('\n')
+        if next_index == -1 or len(text) <= max_length:
+            next_index = max_length
+        parts.append(text[:next_index])
+        text = text[next_index:].lstrip()
+
+    return parts
+
+
+def translate_text(parts, target_language='ko'):
+    translator = GoogleTranslator(source='auto', target=target_language)
+    return [translator.translate(part) for part in parts]
+
+def save_translated_text(translated_parts, output_path):
+    with open(output_path, 'w', encoding='utf-8') as file:
+        file.write('\n'.join(translated_parts))
+
+
 def get_all_files_in_directory(directory):
     all_files = []
 
@@ -65,4 +90,22 @@ for root, dirs, files in os.walk(directory):
                        "max_line_count": 10000, "highlight_words": True}
             writer(result, video_file_path, options)
             path = os.path.realpath(directory)
+            os.startfile(path)
+        elif os.path.splitext(filename)[1] in ['.txt']:
+            doc_file_path = os.path.join(root, filename )
+            # file_size_mb = os.path.getsize(
+            #     doc_file_path) / (1024 * 1024)  # File size in megabytes
+
+            doc_file_path = os.path.join(root, filename)
+
+            # 파일 분할 및 번역
+            parts = split_text(doc_file_path)
+            translated_parts = translate_text(parts)
+
+            # 번역된 텍스트 저장
+            result_path = os.path.join(root, "ko_" + filename)
+            save_translated_text(translated_parts, result_path)
+
+            # 완료된 디렉토리 열기
+            path = os.path.realpath(root)
             os.startfile(path)
